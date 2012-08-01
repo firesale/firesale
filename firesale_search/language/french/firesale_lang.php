61eeddd31742dda35209673cfe540ba696ca01ab
Dixens <nicolasdupuy@dixens.net>
2012-08-01 16:15:24 +0200

    French translation

---
+++ firesale_search/language/french/firesale_lang.php
@@ -0,0 +1,10 @@
+<?php
+	
+	$lang['firesale:test:label_hello'] 		  = 'Hello World!';
+	$lang['firesale:sections:search'] 		  = 'Recherche';
+	$lang['firesale:sections:search_results'] = 'Résultats de recherche correspondant à "%s"';
+	$lang['firesale:search:products_match']	  = 'Produits correspondant aux critères de recherche';
+	$lang['firesale:search:products_none']	  = 'Aucun produit ne correspond aux critères de recherche';
+	
+	// Dashboard
+	$lang['firesale:elements:search_terms'] = 'Termes de recherches populaires';
