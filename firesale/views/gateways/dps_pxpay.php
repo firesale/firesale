b7ded9a4ef085af9f5543a6dc9194168efe251ed
Chris Harvey <chris@chrisnharvey.com>
2012-08-01 13:20:36 +0100

    Adding default payment gateway views

---
+++ firesale/views/gateways/dps_pxpay.php
@@ -0,0 +1,11 @@
+<?php echo form_open(); ?>
+	<?php echo form_hidden('return_url', site_url('firesale/cart/payment')); ?>
+	<?php echo form_hidden('cancel_url', site_url('firesale/cart/cancel')); ?>
+	<?php echo form_hidden('reference', 'Test Order'); ?>
+	<?php echo form_hidden('currency_code', 'GBP'); ?>
+	<?php echo form_hidden('transaction_id', $this->session->userdata('order_id')); ?>
+	<?php echo form_hidden('email', $this->current_user->email); ?>
+	
+	<p>Payment processing is done on the eWAY website, so if you're happy with everything please click below to finalise your order!</p>
+	<button type="submit" class="btn"><span>Pay with eWAY</span></button>
+<?php echo form_close(); ?>
