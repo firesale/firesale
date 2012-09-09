949c168f0ca96187a92d26c853460d6e14a810b8
toni haryanto <toha.samba@gmail.com>
2012-09-09 10:08:57 +0700

    add Indonesian language

---
+++ firesale/language/indonesian/permission_lang.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['firesale.role_edit_orders'] = 'Edit Pesanan';
+$lang['firesale.role_access_gateways'] = 'Akses Gateway';
+$lang['firesale.role_install_uninstall_gateways'] = 'Pasang/Copot Gateway';
+$lang['firesale.role_enable_disable_gateways'] = 'Aktif/Nonaktif Gateway';
+$lang['firesale.role_edit_gateways'] = 'Edit Gateway';
