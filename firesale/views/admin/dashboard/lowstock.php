cd3c020ccc7269f8f0ace239bb392a9639a4455d
Christian Giupponi <christian.giupponi@stilogo.it>
2012-09-13 18:09:51 +0200

    update "view more" and "edit" label
    update "view more" and "edit" label, in this way it can load the
    correct language

--- firesale/views/admin/dashboard/lowstock.php
+++ firesale/views/admin/dashboard/lowstock.php
@@ -22,13 +22,13 @@
                 <td><?php echo $product['code']; ?></td>
                 <td><a href="{{ url:base }}product/<?php echo $product['slug']; ?>"><?php echo $product['title']; ?></a></td>
                 <td><?php echo $product['stock']; ?></td>
-                <td><a href="/admin/firesale/products/edit/<?php echo $product['id']; ?>" class="btn green"><span>Edit</span></td>
+                <td><a href="/admin/firesale/products/edit/<?php echo $product['id']; ?>" class="btn green"><span><?php echo lang('global:edit'); ?></span></td>
               </tr>
 <?php endforeach; ?>
             </tbody>
           </table>
           <br />
-          <a href="{{ url:base }}admin/firesale/products/stock_status/2" class="btn blue"><span>View More</span></a>
+          <a href="{{ url:base }}admin/firesale/products/stock_status/2" class="btn blue"><span><?php echo lang('firesale:dashboard:view_more'); ?></span></a>
 <?php else: ?>
           <div class="no_data"><?php echo lang('firesale:dashboard:no_stock_low'); ?></div>
 <?php endif; ?>
@@ -51,13 +51,13 @@
                 <td><?php echo $product['code']; ?></td>
                 <td><a href="{{ url:base }}product/<?php echo $product['slug']; ?>"><?php echo $product['title']; ?></a></td>
                 <td><?php echo $product['stock']; ?></td>
-                <td><a href="/admin/firesale/products/edit/<?php echo $product['id']; ?>" class="btn green"><span>Edit</span></td>
+                <td><a href="/admin/firesale/products/edit/<?php echo $product['id']; ?>" class="btn green"><span><?php echo lang('global:edit'); ?></span></td>
              </tr>
 <?php endforeach; ?>
             </tbody>
           </table>
           <br />
-          <a href="{{ url:base }}admin/firesale/products/stock_status/3" class="btn blue"><span>View More</span></a>
+          <a href="{{ url:base }}admin/firesale/products/stock_status/3" class="btn blue"><span><?php echo lang('firesale:dashboard:view_more'); ?></span></a>
 <?php else: ?>
           <div class="no_data"><?php echo lang('firesale:dashboard:no_stock_out'); ?></div>
 <?php endif; ?>
