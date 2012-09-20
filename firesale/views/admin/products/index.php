2d8d4a6315c111d3f68bb16ab8044ef7d6fdd735
jHoldroyd <apocalyps3@gmail.com>
2012-09-20 16:35:45 +0100

    Closes issue #91

--- firesale/views/admin/products/index.php
+++ firesale/views/admin/products/index.php
@@ -46,7 +46,7 @@
 					<tr class="cat_<?php echo $product['category']['id']; ?>">
 						<td><input type="checkbox" name="action_to[]" value="<?php echo $product['id']; ?>"  /></td>
 						<td class="item-id"><?php echo $product['code']; ?></td>
-						<td class="item-img"><img src="<?php echo ( $product['image'] != FALSE ? '/files/thumb/' . $product['image'] . '/32/32' : '' ); ?>" alt="Product Image" /></td>
+						<td class="item-img"><img src="<?php echo ( $product['image'] != FALSE ? site_url('files/thumb/' . $product['image'] . '/32/32') : '' ); ?>" alt="Product Image" /></td>
 						<td class="item-title"><a href="/product/<?php echo $product['slug']; ?>"><?php echo $product['title']; ?></a></td>
 						<td class="item-category">
 							<?php $string = ''; foreach( $product['category'] AS $cat ) { $string .= ( strlen($string) == 0 ? '' : ', ' ) . '<span data-id="' . $cat['id'] . '">' . $cat['title'] . '</span>'; } echo $string; ?>
