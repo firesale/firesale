0fa80e553014ad4e9b2fbe9f7c82c6df2a0dd400
Jamie Holdroyd <apocalyps3@gmail.com>
2012-09-22 22:52:27 +0100

    Updating readme/details

--- firesale/details.php
+++ firesale/details.php
@@ -2,7 +2,7 @@
 
 class Module_Firesale extends Module {
 	
-	public $version = '1.0.4';
+	public $version = '1.1.0';
 	public $language_file = 'firesale/firesale';
 	
 	public function __construct()
