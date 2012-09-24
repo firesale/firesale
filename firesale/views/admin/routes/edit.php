69486ace1e2b372fb0a08f4d32875e830dca1622
Jamie Holdroyd <apocalyps3@gmail.com>
2012-09-24 07:45:39 +0100

    Added base of edit page

--- firesale/views/admin/routes/edit.php
+++ firesale/views/admin/routes/edit.php
@@ -1,1 +1,30 @@
-Edit
+
+	<section class="title">
+		<h4><?php echo lang('firesale:routes:edit'); ?></h4>
+	</section>
+
+	<section class="item form_inputs">
+
+		<?php echo form_open_multipart($this->uri->uri_string(), 'class="crud"'); ?>
+
+			<fieldset>
+				<ul>
+				<?php foreach( $fields AS $input ): ?>
+					<li class="<?php echo alternator('even', ''); ?>">
+						<label for="<?php echo $input['input_slug']; ?>">
+							<?php echo lang(substr($input['input_title'], 5)); ?>
+							<small><?php echo lang(substr($input['instructions'], 5)); ?></small>
+						</label>
+						<div class="input"><?php echo $input['input']; ?></div>
+					</li>
+				<?php endforeach; ?>
+				</ul>
+			</fieldset>
+
+			<div class="buttons">
+				<?php $this->load->view('admin/partials/buttons', array('buttons' => array('save', 'save_exit', 'cancel') )); ?>
+			</div>
+
+		<?php echo form_close(); ?>
+
+	</section>
