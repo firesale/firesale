b0311545123d98c5056b8aa3818ae8dde6c56498
Jamie Holdroyd <apocalyps3@gmail.com>
2012-09-27 19:00:01 +0100

    Updated/added routes language

---
+++ firesale/language/italian/permission_lang.php
@@ -0,0 +1,9 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['firesale.role_edit_orders']                = 'Edit Orders'; # Translate
+$lang['firesale.role_access_gateways']            = 'Access Gateways'; # Translate
+$lang['firesale.role_install_uninstall_gateways'] = 'Install/Uninstall Gateways'; # Translate
+$lang['firesale.role_enable_disable_gateways']    = 'Enable/Disable Gateways'; # Translate
+$lang['firesale.role_edit_gateways']              = 'Edit Gateways'; # Translate
+$lang['firesale.role_access_routes']              = 'Access Routes'; # Translate
+$lang['firesale.create_edit_routes']              = 'Create/Edit Routes'; # Translate
