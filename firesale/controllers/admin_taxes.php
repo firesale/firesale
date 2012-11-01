a883e3508fee453d9655d18b0e3978a42fca4d3e
Chris Harvey <chris@chrisnharvey.com>
2012-11-01 15:19:12 +0000

    Added taxes index method to controller

--- firesale/controllers/admin_taxes.php
+++ firesale/controllers/admin_taxes.php
@@ -14,6 +14,15 @@ class Admin_taxes extends Admin_Controller
 
 	public function index()
 	{
-		
+		$params = array(
+			'stream'       => 'firesale_taxes',
+			'namespace'    => 'firesale_taxes',
+			'paginate'     => 'yes',
+			'page_segment' => 4
+		);
+
+        $data['taxes'] = $this->streams->entries->get_entries($params);
+
+        $this->template->build('admin/taxes/index', $data);
 	}
 }
