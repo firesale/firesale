b5a7bbce9a36d17bdd3ec989d8ebf60a2f25534d
Chris Harvey <chris@chrisnharvey.com>
2012-11-08 10:35:44 +0000

    Fixing issue where save button was disabled

--- firesale/views/admin/taxes/assign.php
+++ firesale/views/admin/taxes/assign.php
@@ -6,31 +6,31 @@
 
 	<section class="item form_inputs">
 
-			<div class="form_inputs">
-				<table id="tax_assignments">
-					<thead>
+		<div class="form_inputs">
+			<table id="tax_assignments">
+				<thead>
+					<tr>
+						<td class="spacer" style="width:100px"></td>
+						<?php foreach ($taxes as $tax): ?>
+							<td id="<?php echo $tax['id']; ?>" data-title="<?php echo $tax['title']; ?>"><?php echo $tax['title']; ?></td>
+						<?php endforeach; ?>
+					</tr>
+				</thead>
+				<tbody>
+					<?php foreach ($currencies as $currency): ?>
 						<tr>
-							<td class="spacer" style="width:100px"></td>
-							<?php foreach ($taxes as $tax): ?>
-								<td id="<?php echo $tax['id']; ?>" data-title="<?php echo $tax['title']; ?>"><?php echo $tax['title']; ?></td>
+							<td><strong><?php echo $currency['title']; ?> (<?php echo $currency['cur_code']; ?>)</strong></td>
+							<?php foreach ($currency['taxes'] as $tax): ?>
+								<td><?php echo form_input('assignment[' . $currency['id'] . '][' . $tax['id'] . ']', $tax['value'] ? $tax['value'] : number_format($currency['cur_tax'], 2)); ?></td>
 							<?php endforeach; ?>
 						</tr>
-					</thead>
-					<tbody>
-						<?php foreach ($currencies as $currency): ?>
-							<tr>
-								<td><strong><?php echo $currency['title']; ?> (<?php echo $currency['cur_code']; ?>)</strong></td>
-								<?php foreach ($currency['taxes'] as $tax): ?>
-									<td><?php echo form_input('assignment[' . $currency['id'] . '][' . $tax['id'] . ']', $tax['value'] ? $tax['value'] : number_format($currency['cur_tax'], 2)); ?></td>
-								<?php endforeach; ?>
-							</tr>
-						<?php endforeach; ?>
-					</tbody>
-				</table>
-			</div>
+					<?php endforeach; ?>
+				</tbody>
+			</table>
+		</div>
 
-		<div class="buttons">
-			<?php $this->load->view('admin/partials/buttons', array('buttons' => array('save', 'cancel') )); ?>
+		<div class="actions">
+			<?php $this->load->view('admin/partials/buttons', array('buttons' => array('save') )); ?>
 		</div>
 
 	</section>
