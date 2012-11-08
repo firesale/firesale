d64b9a8a50dc86a3a213d2b70c727a4a96552312
Chris Harvey <chris@chrisnharvey.com>
2012-11-08 10:07:01 +0000

    Tax assignments now default to the currency tax rate

--- firesale/views/admin/taxes/assign.php
+++ firesale/views/admin/taxes/assign.php
@@ -21,7 +21,7 @@
 							<tr>
 								<td><strong><?php echo $currency['title']; ?> (<?php echo $currency['cur_code']; ?>)</strong></td>
 								<?php foreach ($currency['taxes'] as $tax): ?>
-									<td><?php echo form_input('assignment[' . $currency['id'] . '][' . $tax['id'] . ']', $tax['value']); ?></td>
+									<td><?php echo form_input('assignment[' . $currency['id'] . '][' . $tax['id'] . ']', $tax['value'] ? $tax['value'] : number_format($currency['cur_tax'], 2)); ?></td>
 								<?php endforeach; ?>
 							</tr>
 						<?php endforeach; ?>
