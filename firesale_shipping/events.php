91d277661d61446057941ce0c5af81e2f8dd9bbb
jHoldroyd <apocalyps3@gmail.com>
2012-11-09 15:10:34 +0000

    Stopped shipping tab appearing on categories

--- firesale_shipping/events.php
+++ firesale_shipping/events.php
@@ -18,14 +18,20 @@ class Events_Firesale_shipping
     public function form_build($controller)
     {
 
-		// Remove images (needs to be last)
-	    unset($controller->tabs['_images']);
+		// Check we're in products
+		if( isset($controller->section) AND $controller->section == 'products' )
+		{
 
-	    // Add metadata to tabs
-	    $controller->tabs['shipping'] = array('ship_req', 'shipping_weight', 'shipping_height', 'shipping_width', 'shipping_depth');
+			// Remove images (needs to be last)
+		    unset($controller->tabs['_images']);
 
-	    // Add images back in
-	    $controller->tabs['_images'] = array();
+		    // Add metadata to tabs
+		    $controller->tabs['shipping'] = array('ship_req', 'shipping_weight', 'shipping_height', 'shipping_width', 'shipping_depth');
+
+		    // Add images back in
+		    $controller->tabs['_images'] = array();
+
+		}
 
     }
 	
