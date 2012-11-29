710beb8930943300427ec124379fbfd53e8925b9
jHoldroyd <apocalyps3@gmail.com>
2012-11-29 09:24:18 +0000

    Updated brands plugin

--- firesale_brands/plugin.php
+++ firesale_brands/plugin.php
@@ -8,10 +8,56 @@ class Plugin_Firesale_brands extends Plugin
 
     	// Load requirements
 		$this->load->library('files/files');
-		$this->load->model('firesale/categories_m');
-		$this->load->model('firesale/products_m');
-		$this->load->model('firesale/routes_m');
-		$this->load->model('firesale/taxes_m');
-		$this->load->model('firesale/currency_m');
+		$this->load->model('brands_m');
 
 	}
+
+	public function get()
+	{
+
+		// Variables
+		$attributes = $this->attributes();
+
+		// Build query
+		$query = $this->db->select('b.id')
+						  ->from('firesale_brands AS b')
+						  ->where('b.status', '1');
+
+		// Add to query
+		foreach( $attributes AS $key => $val )
+		{
+
+			switch($key)
+			{
+
+				case 'limit':
+					$query->limit($val);
+				break;
+
+				case 'order':
+					list($by, $dir) = explode(' ', $val);
+					$query->order_by('b.' . $by, $dir);
+				break;
+
+				default:
+					$query->where('b.'.$key, $val);
+				break;
+
+			}
+
+		}
+
+		// Run query
+		$results = $query->get()->result_array();
+
+		// Get brands
+		foreach( $results AS &$result )
+		{
+			$result = $this->brands_m->get($result['id']);
+		}
+
+		// Return results
+		return $results;
+	}
+
+}
