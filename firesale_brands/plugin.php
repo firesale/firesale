11bd6dc758506d90fd9eaddf2753850c947c3a91
AJSturrock <adam@adamsturrock.co.uk>
2013-01-24 15:22:15 +0000

    Added caching again

--- firesale_brands/plugin.php
+++ firesale_brands/plugin.php
@@ -17,43 +17,50 @@ class Plugin_Firesale_brands extends Plugin
 
         // Variables
         $attributes = $this->attributes();
+        $cache_key  = md5(BASE_URL.implode('|', $attributes));
 
-        // Build query
-        $query = $this->db->select('id')
-                          ->where('status', '1');
+        if ( ! $brands = $this->cache->get($cache_key) ) {
+            // Build query
+            $query = $this->db->select('id')
+                              ->where('status', '1');
 
-        // Add to query
-        foreach( $attributes AS $key => $val ) {
+            // Add to query
+            foreach ($attributes AS $key => $val) {
 
-            switch($key) {
+                switch ($key) {
 
-                case 'limit':
-                    $query->limit($val);
-                break;
+                    case 'limit':
+                        $query->limit($val);
+                    break;
 
-                case 'order':
-                    list($by, $dir) = explode(' ', $val);
-                    $query->order_by($by, $dir);
-                break;
+                    case 'order':
+                        list($by, $dir) = explode(' ', $val);
+                        $query->order_by($by, $dir);
+                    break;
 
-                default:
-                    $query->where($key, $val);
-                break;
+                    default:
+                        $query->where($key, $val);
+                    break;
+
+                }
 
             }
 
-        }
+            // Run query
+            $brands = $query->get('firesale_brands')->result_array();
 
-        // Run query
-        $results = $query->get('firesale_brands')->result_array();
+            // Get brands
+            foreach ($brands AS &$result) {
+                $result = $this->pyrocache->model('brands_m', 'get', ($result['id']), $this->firesale->cache_time);
+            }
 
-        // Get brands
-        foreach( $results AS &$result ) {
-            $result = $this->brands_m->get($result['id']);
+            // Add to cache
+            $this->cache->save($cache_key, $brands, $this->firesale->cache_time);
+
         }
 
         // Return results
-        return $results;
+        return $brands;
     }
 
 }
