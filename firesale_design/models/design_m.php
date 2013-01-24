25d066c3d2cc8d25fc900ca08b377611566f591b
jHoldroyd <apocalyps3@gmail.com>
2013-01-24 15:20:27 +0000

    May have missed these

--- firesale_design/models/design_m.php
+++ firesale_design/models/design_m.php
@@ -3,5 +3,54 @@
 class Design_m extends MY_Model
 {
 
+    public function get_theme_views($theme)
+    {
+
+        $views = array();
+
+        foreach ($this->template->theme_locations() as $location) {
+            if(is_dir($location.$theme.'/views/modules')) {
+                foreach(glob($location.$theme.'/views/modules/firesale*') as $module) {
+                    $module = pathinfo($module, PATHINFO_BASENAME);
+                    foreach(glob($location.$theme.'/views/modules/'.$module.'/*.*') as $view) {
+                        $views[] = $module.'/'.pathinfo($view, PATHINFO_BASENAME);
+                    }
+                }
+                break;
+            }
+        }
+
+        return $views;
+    }
+
+    public function get_design($type, $id)
+    {
+
+        $query = $this->db->where('type', $type)
+                          ->where('element', $id)
+                          ->get('firesale_design');
+
+        if( $query->num_rows() ) {
+            return current($query->result_array());
+        }
+
+        return false;
+    }
+
+    public function format_name($string)
+    {
+
+        // Variables
+        $replace = array('_', '.php', '.html');
+        $with    = array(' ', '', '');
+
+        // Format
+        $string = explode('/', $string);
+        $string = end($string);
+        $string = str_replace($replace, $with, $string);
+
+        // Return
+        return ucwords($string);
+    }
 
 }
--- firesale_design/plugin.php
+++ firesale_design/plugin.php
@@ -9,21 +9,40 @@ class Plugin_Firesale_design extends Plugin
         // Load required items
         $this->load->model('firesale_design/design_m');
 
-        // Variables
-        $type = $this->attribute('type', 'product');
-        $id   = (int) $this->attribute('id');
+        // Attributes
+        $type = $this->attribute('type');
+        $id   = (int)$this->attribute('id');
 
-        // Build data
-        $data          = new stdClass;
-        $data->layouts = $this->template->get_layouts();
+        // Check attributes
+        if( $type and $id ) {
 
-        // Format names
-        foreach( $data->layouts as &$layout ) {
-            $layout = ucwords(str_replace(array('_', '.php', '.html'), array(' ', '', ''), $layout));
+            $theme   = $this->settings->get('default_theme');
+            $layouts = $this->template->get_theme_layouts($theme);
+            $views   = $this->design_m->get_theme_views($theme);
+
+            // Build data
+            $data          = new stdClass;
+            $data->type    = $type;
+            $data->id      = $id;
+            $data->layouts = array();
+            $data->views   = array();
+            $data->design  = $this->design_m->get_design($type, $id);
+
+            // Format layout names
+            foreach( $layouts as $layout ) {
+                $data->layouts[$layout] = $this->design_m->format_name($layout);
+            }
+
+            // Format view names
+            foreach( $views as $view ) {
+                $data->views[$view] = $this->design_m->format_name($view);
+            }
+
+            // Build form
+            return $this->parser->parse('firesale_design/form', $data, true);
         }
 
-        // Build form
-        return $this->parser->parse('firesale_design/form', $data, true);
+        return false;
     }
 
 }
