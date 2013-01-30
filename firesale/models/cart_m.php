dcf2921241b0b93bfefeda5fc671cc404d89c1d9
Adam Sturrock <adam@adamsturrock.co.uk>
2013-01-30 15:17:22 +0000

    Fixed cart insert for product variants

--- firesale/models/cart_m.php
+++ firesale/models/cart_m.php
@@ -83,7 +83,7 @@ class Cart_m extends MY_Model
         foreach ($products AS $row_id => $item) {
 
             // Get product
-            $product = $this->pyrocache->model('products_m', 'get_product', array('id' => $item['id']), $this->firesale->cache_time);
+            $product = $this->pyrocache->model('products_m', 'get_product', array($item['id'], NULL, 1), $this->firesale->cache_time);
 
             // Check prices
             if (round($item['price'], 1) != round($product['price'], 1)) {
