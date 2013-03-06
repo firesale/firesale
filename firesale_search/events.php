a8951486f7f57daad588bdb3eb0ddfd527c6541d
jHoldroyd <apocalyps3@gmail.com>
2013-03-06 13:56:28 +0000

    Updated dashboard event

--- firesale_search/events.php
+++ firesale_search/events.php
@@ -13,12 +13,27 @@ class Events_Firesale_search
         $this->ci->load->model('firesale_search/search_m');
 
         Events::register('clear_cache', array($this, 'clear_cache'));
-
+        Events::register('firesale_dashboard', array($this, 'firesale_dashboard_search'));
     }
 
     public function clear_cache()
     {
         $this->ci->pyrocache->delete_all('search_m');
+    }
+
+    public function firesale_dashboard_search()
+    {
+
+        // Get search results
+        $results = $this->ci->db->order_by('sales, count')->limit(10)->get('firesale_search')->result_array();
+
+        // Build and return data
+        return array(
+            'id'      => 'search',
+            'title'   => lang('firesale:sections:search'),
+            'content' => $this->ci->parser->parse('firesale_search/admin_searchterms', array('results' => $results), true),
+            'assets'  => array()
+        );
     }
 
 }
--- firesale_search/plugin.php
+++ firesale_search/plugin.php
@@ -3,17 +3,6 @@
 class Plugin_Firesale_Search extends Plugin
 {
 
-    #####################
-    ## ADMIN DASHBOARD ##
-    #####################
-
-    public function search_terms()
-    {
-
-        $results = $this->db->order_by('sales, count')->limit(10)->get('firesale_search')->result_array();
-
-        // Return view
-        return $this->module_view('firesale_search', 'admin_searchterms', array('results' => $results), true);
-    }
+	
 
 }
