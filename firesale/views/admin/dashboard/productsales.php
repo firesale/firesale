c902a12e69608b1cd4759c3969e99985c7a73c72
jHoldroyd <apocalyps3@gmail.com>
2013-03-08 12:08:33 +0000

    Fixed sales graph on dashboard

--- firesale/views/admin/dashboard/productsales.php
+++ firesale/views/admin/dashboard/productsales.php
@@ -1,14 +1,14 @@
 
 <?php if( isset($total_count) && $total_count > 0 ): ?>
 
-      <div id="sales"></div>
+      <div id="sales_graph"></div>
 
     <script type="text/javascript">
         var currency = '<?php echo $currency; ?>';
         var sales    = <?php echo $sales; ?>;
         var count    = <?php echo $count; ?>;
-        buildGraph(sales, count, '#sales');
-        $(window).resize(function() { buildGraph(sales, count, '#sales'); });
+        buildGraph(sales, count, '#sales_graph');
+        $(window).resize(function() { buildGraph(sales, count, '#sales_graph'); });
     </script>
 
 <?php else: ?>
