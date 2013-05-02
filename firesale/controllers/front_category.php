4747975964e29c7acecb29ac75ca6c5b92539c15
Adam Sturrock <adam@adamsturrock.co.uk>
2013-05-02 13:39:14 +0100

    Category breadcrumbs should now be correct (last uri segment should not have a link)

--- firesale/controllers/front_category.php
+++ firesale/controllers/front_category.php
@@ -127,12 +127,19 @@ class Front_category extends Public_Controller
             // Breadcrumbs
             if ( $category == null ) {
                 $url  = $this->pyrocache->model('routes_m', 'build_url', array('category', NULL), $this->firesale->cache_time);
-                $this->template->set_breadcrumb(lang('firesale:cats_all_products'), $url);
+                $this->template->set_breadcrumb(lang('firesale:cats_all_products'));
             } else {
                 $cats = $this->pyrocache->model('products_m', 'get_cat_path', array($category['id'], true), $this->firesale->cache_time);
                 foreach ($cats as $key => $cat) {
                     $url = $this->pyrocache->model('routes_m', 'build_url', array('category', $cat['id']), $this->firesale->cache_time);
-                    $this->template->set_breadcrumb($cat['title'], $url);
+                    if ($url == uri_string())
+                    {
+                        $this->template->set_breadcrumb($cat['title']);
+                    }
+                    else
+                    {
+                        $this->template->set_breadcrumb($cat['title'], $url);
+                    }
                 }
             }
 
