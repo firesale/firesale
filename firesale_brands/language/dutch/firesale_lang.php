3f82639d9d34125f6eabb7963d764f4fd14d1f47
Jacob Albert Jolman <jaap@jolman.eu>
2013-05-22 18:35:37 +0200

    More Dutch Language

---
+++ firesale_brands/language/dutch/firesale_lang.php
@@ -0,0 +1,41 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+* This file is part of FireSale, a PHP based eCommerce system built for
+* PyroCMS.
+*
+* Copyright (c) 2013 Moltin Ltd.
+* http://github.com/firesale/firesale
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*
+* @package firesale/brands
+* @author FireSale <support@getfiresale.org>
+* @copyright 2013 Moltin Ltd.
+* @version master
+* @link http://github.com/firesale/firesale
+*
+*/
+
+    // Sections
+    $lang['firesale:sections:brands'] = 'Merken';
+
+    // Shortcuts
+    $lang['firesale:shortcuts:brand_create'] = 'Merk Toevoegen';
+
+    // Brands
+    $lang['firesale:brands:title']          = 'Merken';
+    $lang['firesale:brands:new']            = 'Een nieuw merk toevoegen';
+    $lang['firesale:brands:add_success']    = 'Merk met succes aangemaakt';
+    $lang['firesale:brands:add_error']      = 'Fout bij het toevoegen van een nieuw merk';
+    $lang['firesale:brands:edit']           = 'Merk %s Bewerken';
+    $lang['firesale:brands:edit_success']   = 'Merk met succes bijgewerkt';
+    $lang['firesale:brands:edit_error']     = 'Fout bij het bewerken van een nieuw merk';
+    $lang['firesale:brands:not_found']      = 'Het gekozen merk bestaat niet';
+    $lang['firesale:brands:none']           = 'Geen Merken Gevonden';
+    $lang['firesale:brands:delete_success'] = 'Merk met succes verwijderd';
+    $lang['firesale:brands:delete_error']   = 'Fout bij het verwijderen van een nieuw merk';
+
+    // Labels
+    $lang['firesale:label_brand'] = 'Merk';
---
+++ firesale_design/language/dutch/firesale_lang.php
@@ -0,0 +1,38 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+* This file is part of FireSale, a PHP based eCommerce system built for
+* PyroCMS.
+*
+* Copyright (c) 2013 Moltin Ltd.
+* http://github.com/firesale/firesale
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*
+* @package firesale/design
+* @author FireSale <support@getfiresale.org>
+* @copyright 2013 Moltin Ltd.
+* @version master
+* @link http://github.com/firesale/firesale
+*
+*/
+
+    // General
+    $lang['firesale:design:title'] = 'FireSale Ontwerp';
+    $lang['firesale:tabs:design']  = 'Ontwerp';
+
+    // Pages
+    $lang['firesale:design:pages:products']   = 'Producten';
+    $lang['firesale:design:pages:categories'] = 'Categorieën';
+    $lang['firesale:design:pages:brands']     = 'Merken';
+
+    // Settings
+    $lang['firesale:design:settings:enable'] = 'Ontwerp opties inschakelen voor';
+
+    // Labels
+    $lang['firesale:design:label_enable']  = 'Ontwerp Elementen Inschakelen';
+    $lang['firesale:design:label_element'] = 'Element';
+    $lang['firesale:design:label_type']    = 'Type';
+    $lang['firesale:design:label_layout']  = 'Layout';
+    $lang['firesale:design:label_view']    = 'View';
