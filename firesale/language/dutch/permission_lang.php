8b0a52e219a1499f6cd48d66a34ed8b7c17a99b9
Jacob Albert Jolman <jaap@jolman.eu>
2013-05-22 16:19:12 +0200

    Dutch Language

---
+++ firesale/language/dutch/permission_lang.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['firesale:role_edit_orders']                = 'Orders Bewerken';
+$lang['firesale:role_access_gateways']            = 'Toegang tot de betalingspoorten';
+$lang['firesale:role_access_taxes']               = 'Toegang tot de BTW tarieven';
+$lang['firesale:role_add_edit_taxes']             = 'Aanmaken/Bewerken van de BTW tarieven';
+$lang['firesale:role_install_uninstall_gateways'] = 'Installeren/deinstalleren van de betalingspoorten';
+$lang['firesale:role_enable_disable_gateways']    = 'Inschakelen/Uitschakelen van de betalingspoorten';
+$lang['firesale:role_edit_gateways']              = 'Bewerken van de betalingspoorten';
+$lang['firesale:role_access_routes']              = 'Toegang tot de routes';
+$lang['firesale:role_create_edit_routes']         = 'Aanmaken/Bewerken van de routes';
+$lang['firesale:role_access_currency']            = 'Toegang tot valuta';
+$lang['firesale:role_install_uninstall_currency'] = 'Installeren/deinstalleren van valuta\'s';
