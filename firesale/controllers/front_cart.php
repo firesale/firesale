1d50ab48ebb0bcd128793a179848acac1fe10a16
Giles Smith <giles@superrb.com>
2013-05-29 11:37:12 +0000

    Added a new method to retrieve cart contents as a json object

--- firesale/controllers/front_cart.php
+++ firesale/controllers/front_cart.php
@@ -86,7 +86,41 @@ class Front_cart extends Public_Controller
 
         // Check for price change
         $this->cart_m->check_price();
+        
+        // get the cart data
+        $data = $this->_cart_data();
 
+        // Add page data
+        $this->template->set_breadcrumb(lang('firesale:cart:title'))
+                       ->title(lang('firesale:cart:title'));
+
+        // Fire events
+        Events::trigger('page_build', $this->template);
+
+        // Build page
+        $this->template->build('cart', $data);
+    }
+    
+    /**
+     * Return the cart as a json object
+     */
+    public function json()
+    {
+        // Check for price change
+        $this->cart_m->check_price();
+        
+        // get the cart data
+        $data = $this->_cart_data();
+        
+        // output json response
+        $this->output->set_content_type('application/json')
+                ->set_output(json_encode($data));
+    }
+    
+    public function _cart_data()
+    {
+        $data = array();
+        
         // Assign Variables
         $data['subtotal']    = $this->currency_m->format_string($this->fs_cart->subtotal(), $this->fs_cart->currency(), false);
         $data['tax']   		 = $this->currency_m->format_string($this->fs_cart->tax(), $this->fs_cart->currency(), false);
@@ -104,16 +138,8 @@ class Front_cart extends Public_Controller
 
             $i++;
         }
-
-        // Add page data
-        $this->template->set_breadcrumb(lang('firesale:cart:title'))
-                       ->title(lang('firesale:cart:title'));
-
-        // Fire events
-        Events::trigger('page_build', $this->template);
-
-        // Build page
-        $this->template->build('cart', $data);
+        
+        return $data;
     }
 
     public function insert($prd_code = NULL, $qty = 1)
