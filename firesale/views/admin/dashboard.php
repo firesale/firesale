0f340ff21bfd45f9d268ffc14cd25e15e2936c41
Pavol Tanuska <pavol.tanuska@gmail.com>
2013-07-10 16:03:06 +0200

    Refixed issue #389 - missing echo calls after changing <?= to <?php

--- firesale/views/admin/dashboard.php
+++ firesale/views/admin/dashboard.php
@@ -3,14 +3,14 @@
     <?php if( isset($items) AND ! empty($items) ): ?>
     <?php foreach( $items as $item ): ?>
 
-        <div class="one_half" id="<?php $item['id']; ?>">
+        <div class="one_half" id="<?php echo $item['id']; ?>">
             <section class="draggable title">
-                <h4><?php $item['title']; ?></h4>
+                <h4><?php echo $item['title']; ?></h4>
                 <a class="tooltip-s toggle" title="Toggle this element"></a>
             </section>
-            <section class="item"<?php ( isset($item['hidden']) && $item['hidden'] == true ? ' style="height: auto; display: none"' : '' ); ?>>
+            <section class="item"<?php echo ( isset($item['hidden']) && $item['hidden'] == true ? ' style="height: auto; display: none"' : '' ); ?>>
                 <div class="content">
-                    <?php $item['content']; ?>
+                    <?php echo $item['content']; ?>
                 </div>
             </section>
         </div>
