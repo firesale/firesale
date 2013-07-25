3ccdd213e318ed880b233b037f3522f40c9dcee1
jHoldroyd <apocalyps3@gmail.com>
2013-07-25 12:36:16 +0100

    Updated buy now view

--- firesale/views/partials/buy_now.php
+++ firesale/views/partials/buy_now.php
@@ -3,7 +3,8 @@
 		<?php echo form_open($this->uri->uri_string()); ?>
 			<input type="hidden" name="product" value="<?php echo $product['id']; ?>" />
 			<img src="<?php echo site_url('files/thumb/'.$product['image'].'/160/160'); ?>" alt="<?php echo $product['title']; ?>" />
-			<p><?php echo truncate_words(strip_tags($product['description']), 200); ?></p>
-			<button type="submit" name="btnAction" value="buy_now">Buy Now for <?php echo $product['price_formatted']; ?></button>
+			<p><?php echo truncate_words(strip_tags($product['description']), 400); ?></p>
+			<button type="submit" name="btnAction" value="buy_now"><?php echo sprintf(lang('firesale:label_buy_now_for'), $product['price_formatted']); ?></button>
+			<div class="clearfix"></div>
 		<?php echo form_close(); ?>
 	</div>
