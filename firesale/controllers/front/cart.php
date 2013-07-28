b35281a7bb576e310295c1813598c8a21b4213a9
Adam Sturrock <adam@adamsturrock.co.uk>
2013-07-28 16:44:26 +0100

    address type

--- firesale/controllers/front/cart.php
+++ firesale/controllers/front/cart.php
@@ -609,7 +609,7 @@ class cart extends Public_Controller
 
     public function _validate_address($value)
     {
-        $this->form_validation->set_message('_validate_addess', lang('firesale:checkout:address_invalid'));
+        $this->form_validation->set_message('_validate_address', lang('firesale:checkout:address_invalid'));
 
         return $this->address_m->get_address($value, $this->current_user->id);
     }
